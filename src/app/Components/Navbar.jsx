452cf5f405b9b58c78d44d1a59695c74b9fa04dd
Mubasil <mubasil.emedstore@gmail.com>
2025-07-24 17:27:10 +0530

    Responsive NavBar Create

--- src/app/Components/Navbar.jsx
+++ src/app/Components/Navbar.jsx
@@ -1,22 +1,72 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
-import { Search } from "lucide-react";
+import Link from "next/link";
+import { AlignJustify, Search } from "lucide-react";
 
 const Navbar = () => {
+  const [hide, setHide] = useState(false);
+
+  const manageMenu = () => {
+    setHide((a) => !a);
+  };
   return (
-    <div className="flex justify-between">
-      <div className="Logo">
-        <Image src="/Images/logo.png" alt="" height={30} width={30} />
+    <div className="bg-green-100 py-2">
+      <div className="container mx-auto flex items-center justify-between px-4 md:px-0">
+        <div className="Logo">
+          <Image
+            src="/Images/logo.png"
+            alt=""
+            height={130}
+            width={130}
+            className="object-contain"
+          />
+        </div>
+        <div className="hidden md:flex gap-5 items-center">
+          <Link href="" className="text-green-800 text-lg ">
+            Home
+          </Link>
+          <Link href="" className="text-green-800 text-lg ">
+            Categories
+          </Link>
+          <Link href="" className="text-green-800 text-lg ">
+            Cart
+          </Link>
+          <Link href="" className="text-green-800 text-lg ">
+            Contact
+          </Link>
+        </div>
+
+        <div className="flex items-center gap-4">
+          <Search className="h-6 w-6 cursor-pointer" />
+          <AlignJustify
+            className="h-6 w-6 md:hidden cursor-pointer"
+            onClick={manageMenu}
+          />
+          <Link
+            href=""
+            className="text-white bg-green-700 py-1 px-4 rounded-md"
+          >
+            Login
+          </Link>
+        </div>
       </div>
-      <div>
-        <h6>Home</h6>
-        <h6>Categories</h6>
-        <h6>Cart</h6>
-        <h6>Contact</h6>
-      </div>
-      <div>
-        <Search className="h-8 w-8 " />
-      </div>
+      {hide && (
+        <div className="md:hidden flex flex-col gap-3 px-10 my-8">
+          <Link href="" className="text-green-800 text-lg ">
+            Home
+          </Link>
+          <Link href="" className="text-green-800 text-lg ">
+            Categories
+          </Link>
+          <Link href="" className="text-green-800 text-lg ">
+            Cart
+          </Link>
+          <Link href="" className="text-green-800 text-lg ">
+            Contact
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
