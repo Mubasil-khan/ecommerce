91c7a956e7b2c1fac5c4d512d0c5279db360d0f5
Mubasil <mubasil.emedstore@gmail.com>
2025-07-24 19:36:47 +0530

    Create Banner USing SwiperSlide

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Banner from "./Components/Mainpage/Banner";
 
 export default function Home() {
   return (
     <div >
-      
+      <Banner />
     </div>
   );
 }
